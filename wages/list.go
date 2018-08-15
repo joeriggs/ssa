f0403ee30aae6ea8cf933c87febe33b2b0de14e8
joeriggs <joe.riggs.1962@gmail.com>
2018-08-15 07:21:45 -0400

    Added wages.MostRecentYear().
    We use this to get the last year and wage from a user's statement.
    We then use that wage as we forecast into the future.  All future
    earnings are simulated as being equal to the most recent wage from
    their statement.

--- wages/list.go
+++ wages/list.go
@@ -2,6 +2,19 @@ package wages
 
 type List map[int]float32
 
+func MostRecentYear(list List) (int, float32) {
+	var mostRecentYear int = 0
+	var mostRecentWage float32 = 0.0
+
+	for year, wage := range list {
+		if year > mostRecentYear {
+			mostRecentYear = year
+			mostRecentWage = wage 
+		}
+	}
+	return mostRecentYear, mostRecentWage
+}
+
 func Add(year int, wage float32, list List) {
 	var dup float32 = list[year]
 
